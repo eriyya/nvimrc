54b2b1f30946c959e2220d2b6d1b1b9e1f384548
eriyya <itsrvn.dev@gmail.com>
2026-02-05 12:02:49 +0100

    don't setup inline-fold when in vscode

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -6,20 +6,24 @@
 --   end,
 -- })
 
-require('custom.inline-fold').setup()
+-- inline-fold uses extmarks with inline virt_text and conceal
+-- which causes rendering issues (ghost text) in vscode-neovim
+if not vim.g.vscode then
+  require('custom.inline-fold').setup()
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = {
-    'html',
-    'xhtml',
-    'javascriptreact',
-    'typescriptreact',
-    'vue',
-    'svelte',
-    'astro',
-    'php',
-  },
-  callback = function()
-    require('custom.inline-fold').enable()
-  end,
-})
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = {
+      'html',
+      'xhtml',
+      'javascriptreact',
+      'typescriptreact',
+      'vue',
+      'svelte',
+      'astro',
+      'php',
+    },
+    callback = function()
+      require('custom.inline-fold').enable()
+    end,
+  })
+end
